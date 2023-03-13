3f0b8054495ce23f7a0ff98f26736d63aafb55ed
Sergio Ramirez <sergio@openloophealth.com>
2023-03-12 19:52:49 -0500

    feat: add s3 integration with aws-sdk v3

--- src/image/service/index.ts
+++ src/image/service/index.ts
@@ -1,14 +1,31 @@
-import { S3 } from "aws-sdk";
+import {
+  S3Client,
+  PutObjectCommand,
+  PutObjectCommandInput,
+} from "@aws-sdk/client-s3";
 import { Request, Response } from "express";
 
 export default class ImageService {
-  private s3: any;
+  private s3: S3Client;
 
   constructor() {
-    this.s3 = new S3();
+    this.s3 = new S3Client({ region: "us-east-1" });
   }
 
-  public saveImage(req: Request, res: Response) {
-    res.send("POST /image/save");
+  async storeImage(
+    req: Request<{}, {}, { key: string; body: string }>,
+    res: Response
+  ) {
+    const { key, body } = req.body;
+    const objectParams: PutObjectCommandInput = {
+      Bucket: "nft-image-saver",
+      Body: body,
+      Key: key,
+    };
+
+    const command = new PutObjectCommand(objectParams);
+    await this.s3.send(command);
+
+    res.send("ok");
   }
 }
