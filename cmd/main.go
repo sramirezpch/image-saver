44cb6874e4ccc463cd5863c2c1fc9ec3cf2752bd
Sergio Ramirez <sergio@openloophealth.com>
2023-03-28 00:04:19 -0500

    feat: add s3 client as dependency of image service

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,40 @@
 package main
 
 import (
+	"os"
+
 	"github.com/sramirezpch/image-saver/internal/adapters"
 	"github.com/sramirezpch/image-saver/internal/service"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
+func createAwsSession() (*session.Session, error) {
+	creds := credentials.NewStaticCredentials(os.Getenv("AWS_ACCESS_KEY"), os.Getenv("AWS_SECRET_KEY"), "")
+
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String("us-east-1"),
+		Credentials: creds,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return sess, nil
+}
 func main() {
-	imageService := service.NewService()
+	sess, err := createAwsSession()
+	if err != nil {
+		panic(err)
+	}
+
+	s3Client := s3.New(sess, &aws.Config{Region: aws.String("us-east-1")})
+
+	imageService := service.NewService(s3Client)
 
 	router := adapters.NewRouter(imageService)
 
--- internal/service/image.go
+++ internal/service/image.go
@@ -3,6 +3,7 @@ package service
 import (
 	"log"
 
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,14 +12,14 @@ type SaveImage interface {
 }
 
 type ImageService struct {
+	s3Client *s3.S3
 }
 
-func NewService() *ImageService {
-	return &ImageService{}
+func NewService(s3 *s3.S3) *ImageService {
+	return &ImageService{s3Client: s3}
 }
 
 func (s *ImageService) SaveImage(c *gin.Context) {
-	// Will be receiving the image as a form data
 	form, _ := c.MultipartForm()
 	file := form.File["Image"]
 	title := form.Value["Title"]
